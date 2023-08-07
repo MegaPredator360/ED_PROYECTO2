c464785d5586b82c772ddb497dad6c5126a47dbc
MegaPredator360 <aaronalfarozamora@gmail.com>
2023-08-07 17:28:23 -0600

    Nodos Realizados

--- ED_PROYECTO2/nodos.h
+++ ED_PROYECTO2/nodos.h
@@ -9,8 +9,46 @@ protected:
 	nodos<T>* siguiente;
 
 public:
-	// Contenedor
+	// Constructor
 	nodos(T*, nodos<T>*);
-	~nodo();
+
+	// Destructor
+	virtual ~nodos();
+
+	// Obtendrá el contenido del nodo
+	virtual T* obtenerInfo() const;
+
+	// Obtendrá el contenido del siguiente nodo
+	virtual nodos<T>* obtenerSiguiente() const;
+
+	// Fijará el siguiente nodo
+	virtual void fijarSiguiente(nodos<T>*);
 };
 
+template<class T>
+nodos<T>::nodos(T* _nuevaInfo, nodos<T>* _siguiente)
+{
+	info = _nuevaInfo;
+	this -> siguiente = siguiente;
+}
+
+template<class T>
+nodos<T>::~nodos() { }
+
+template<class T>
+T* nodos<T>::obtenerInfo() const
+{
+	return info;
+}
+
+template<class T>
+nodos<T>* nodos<T>::obtenerSiguiente() const
+{
+	return siguiente;
+}
+
+template<class T>
+void nodos<T>::fijarSiguiente(nodos<T>* _siguiente)
+{
+	this -> siguiente = _siguiente;
+}
