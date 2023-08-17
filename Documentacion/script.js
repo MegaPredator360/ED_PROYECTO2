d435f9ee06c614d5e96d5346d33ffc1febb07ea6
FabianArrieta1 <fabianarrietag@gmail.com>
2023-08-17 01:00:34 -0600

    Actualizacion Documentacion

--- Documentacion/script.js
+++ Documentacion/script.js
@@ -15,6 +15,22 @@ function redirigirBoton2() {
     });
 }
 
+function redirigirBoton3() {
+    var top = document.getElementById("titulo3").offsetTop - document.getElementById("navbar").offsetHeight - 10;
+    window.scrollTo({
+        top: top,
+        behavior: "smooth"
+    });
+}
+
+function redirigirBoton4() {
+    var top = document.getElementById("titulo4").offsetTop - document.getElementById("navbar").offsetHeight - 10;
+    window.scrollTo({
+        top: top,
+        behavior: "smooth"
+    });
+}
+
 // Modo Oscuro
 function toggleDarkMode() {
     var element = document.body;
